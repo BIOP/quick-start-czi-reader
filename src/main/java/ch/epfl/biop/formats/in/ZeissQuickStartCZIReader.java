a85b1f075580b3e2b71673499e33a034430b16e1
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2024-01-04 14:16:22 +0100

    Fix RGB WSI images not having a white background

--- src/main/java/ch/epfl/biop/formats/in/ZeissQuickStartCZIReader.java
+++ src/main/java/ch/epfl/biop/formats/in/ZeissQuickStartCZIReader.java
@@ -305,7 +305,10 @@ public class ZeissQuickStartCZIReader extends FormatReader {
             coreIndexToTZCToMinimalBlocks = new ArrayList<>();
 
     @CopyByRef
-    int nIlluminations, nRotations, nPhases, maxResolution;
+    int nIlluminations, nRotations, nPhases;
+
+    @CopyByRef
+    boolean hasPyramid = false;
 
     // ------------------------ METADATA FIELDS
     @CopyByRef
@@ -884,7 +887,7 @@ public class ZeissQuickStartCZIReader extends FormatReader {
             return fillColor;
         }
         byte fill = (byte) 0;
-        if (isRGB() && (maxResolution > 0)) {
+        if (isRGB() && (hasPyramid)) {
             fill = (byte) 255;
         }
         return fill;
@@ -1197,6 +1200,9 @@ public class ZeissQuickStartCZIReader extends FormatReader {
             Arrays.asList(cziSegments.subBlockDirectory.data.entries).forEach( // and each entry
                     entry -> {
                         int downscalingFactor = (int) Math.round((double)(entry.getDimension("X").size)/(double)(entry.getDimension("X").storedSize));
+
+                        hasPyramid = hasPyramid || (downscalingFactor!=1);
+
                         if ((downscalingFactor==1)||(allowAutostitching())) {
                             // Split by resolution level if flattenedResolutions is true
                             ModuloDimensionEntries moduloEntry = new ModuloDimensionEntries(entry,
@@ -1216,8 +1222,6 @@ public class ZeissQuickStartCZIReader extends FormatReader {
                         }
                     });
         });
-
-        maxResolution = maxValuePerDimension.containsKey(RESOLUTION_LEVEL_DIMENSION)? maxValuePerDimension.get(RESOLUTION_LEVEL_DIMENSION):0; // Used only for auto-determination of the fill color
 
         // Sort them
         List<CoreSignature> orderedCoreSignatureList = coreSignatureToBlocks.keySet().stream().sorted().collect(Collectors.toList());
