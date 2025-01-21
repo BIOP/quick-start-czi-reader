e23c3a512a08df29a05c3983488ede4165543e7d
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2025-01-21 15:28:00 +0100

    Update parent pom to 40.0.0

--- src/test/java/ch/epfl/biop/formats/in/SimpleIJLaunch.java
+++ src/test/java/ch/epfl/biop/formats/in/SimpleIJLaunch.java
@@ -5,13 +5,15 @@ import loci.formats.in.ZeissCZIReader;
 import net.imagej.ImageJ;
 
 public class SimpleIJLaunch {
-    public static void main(String... args) {
+    public static void main(String... args) throws Exception {
         //DebugTools.setRootLevel("OFF");
         //DebugTools.enableLogging("OFF");
 
-        DebugTools.setRootLevel("OFF");
+        DebugTools.setRootLevel("DEBUG");
         ImageJ ij = new ImageJ();
         ij.ui().showUI();
-        ZeissCZIReader r;
+        //ZeissCZIReader r;
+        //ZeissQuickStartCZIReader ro = new ZeissQuickStartCZIReader();
+        //ro.setId("C:\\Users\\chiarutt\\Dropbox\\BIOP\\data-need to fix with new czi reader\\data\\inputs\\DAPI_PGC.czi");
     }
 }
